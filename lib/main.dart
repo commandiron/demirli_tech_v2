4ea501fa5046386c71c77d42858f62aeb1a18b26
commandiron <commandiron@gmail.com>
2023-03-30 13:54:55 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Demirli Tech",
       home: Scaffold(
         backgroundColor: Colors.black,
