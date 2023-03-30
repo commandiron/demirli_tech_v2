c81284f8fe088863b576c731a4fc208ad7c8f9d7
commandiron <commandiron@gmail.com>
2023-03-30 13:52:49 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Demirli Tech",
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/demirli_tech_text_logo_white.png"),
+              const SizedBox(height: 16,),
+              const Text("Yakında..", style: TextStyle(color: Colors.white),)
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
