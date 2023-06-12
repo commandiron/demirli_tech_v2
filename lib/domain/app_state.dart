4c9dc6301c97ff6b785e7dbeb7a5cd85fe61da46
commandiron <commandiron@gmail.com>
2023-06-12 17:13:38 +0300

    .

--- lib/domain/app_state.dart
+++ lib/domain/app_state.dart
@@ -140,15 +140,45 @@ abstract class ProductsAnimationState {
 }
 
 class ProductsAnimationInitial extends ProductsAnimationState{
-  ProductsAnimationInitial({super.titleLineWidth = 0, super.decorativeTitleContainerHeight = 0, super.sliderOffsetY = 1, super.sliderOpacity = 0, super.productItemOpacity = 0, super.backButtonOffsetX = -1, super.nextButtonOffsetX = 1});
+  ProductsAnimationInitial(
+    {
+      super.titleLineWidth = 0,
+      super.decorativeTitleContainerHeight = 0,
+      super.sliderOffsetY = 1,
+      super.sliderOpacity = 0,
+      super.productItemOpacity = 0,
+      super.backButtonOffsetX = -1,
+      super.nextButtonOffsetX = 1
+    }
+  );
 }
 
 class ProductsAnimationStepOne extends  ProductsAnimationState{
-  ProductsAnimationStepOne({super.titleLineWidth = 100, super.decorativeTitleContainerHeight = LayoutDimensions.sectionTitleHeight * 2, super.sliderOffsetY = 0, super.sliderOpacity = 1, super.productItemOpacity = 0, super.backButtonOffsetX = -0.5, super.nextButtonOffsetX = 0.5});
+  ProductsAnimationStepOne(
+    {
+      super.titleLineWidth = 100,
+      super.decorativeTitleContainerHeight = LayoutDimensions.sectionTitleHeight * 2,
+      super.sliderOffsetY = 0,
+      super.sliderOpacity = 1,
+      super.productItemOpacity = 0,
+      super.backButtonOffsetX = -0.5,
+      super.nextButtonOffsetX = 0.5
+    }
+  );
 }
 
 class ProductsAnimationStepTwo extends  ProductsAnimationState{
-  ProductsAnimationStepTwo({super.titleLineWidth = 100, super.decorativeTitleContainerHeight = LayoutDimensions.sectionTitleHeight * 2, super.sliderOffsetY = 0, super.sliderOpacity = 1, super.productItemOpacity = 1, super.backButtonOffsetX = 0, super.nextButtonOffsetX = 0});
+  ProductsAnimationStepTwo(
+    {
+      super.titleLineWidth = 100,
+      super.decorativeTitleContainerHeight = LayoutDimensions.sectionTitleHeight * 2,
+      super.sliderOffsetY = 0,
+      super.sliderOpacity = 1,
+      super.productItemOpacity = 1,
+      super.backButtonOffsetX = 0,
+      super.nextButtonOffsetX = 0
+    }
+  );
 }
 
 abstract class ScrollToTopFabState {
